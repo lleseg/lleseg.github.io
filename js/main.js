ef79fc3b477738592a13ba6ebc33e563bebb23df
Franco De Nardo <fdenardo.utn@gmail.com>
2019-10-10 01:25:07 -0300

    Avances varios, clases 4, 5 y 6

--- js/main.js
+++ js/main.js
@@ -1,49 +1,85 @@
-console.log('Super: Lista de compras');
+// console.log('Super: Lista de compras');
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('../sw.js')
-    .then(function(reg) {
-      console.log('Service Worker registrado exitósamente', reg);
+// Clase 4
+// if (window.caches) {
+//   console.log('El caché existe!');
+
+//   // caches.open('Prueba-1');
+//   // caches.has('Prueba-2').then(existe => console.log(existe));
+//   // caches.has('Prueba-1').then(console.log);
+//   // caches.delete('Prueba-1').then(console.log);
+
+//   caches.open('cache-v1.1').then(cache => {
+//     // cache.add('/index.html');
+//     cache.addAll(['/index.html', '/css/style.css']).then(() => {
+//       console.log('Terminó la carga de cache');
+//       cache.delete('/index.html');
+
+//       cache.match('/css/style.css').then(res => {
+//         res.text().then(console.log);
+//       });
+
+//       cache.put('/css/style.css', new Response('Hello world!'));
+
+//       caches.keys().then(console.log);
+//     });
+//   });
+// }
+
+let listaDeCompras = [
+  //   { producto: 'Fideos', precio: 1 },
+  //   { producto: 'Arroz', precio: 2 },
+  //   { producto: 'Harina', precio: 3 },
+  //   { producto: 'Aceite', precio: 4 },
+];
+
+function setServiceWorker() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('../sw.js')
+      .then(function(reg) {
+        // console.log('Service Worker registrado exitósamente', reg);
+      })
+      .catch(function(err) {
+        console.error('Error registrando el Service Worker', err);
+      });
+  }
+}
+
+function getDatosLista() {
+  $.get('lista.json', function(data) {
+    // console.log(data);
+    // listaDeCompras = data;
+    // renderLista();
+  });
+
+  $.get('https://5d197decb3b6a100148d1fcd.mockapi.io/api/v1/lista', data => {
+    // console.log(data);
+    listaDeCompras = data;
+    localStorage.setItem('lista', JSON.stringify(listaDeCompras));
+    renderLista();
+  })
+    .done(() => {
+      console.log('Success!');
     })
-    .catch(function(err) {
-      console.error('Error registrando el Service Worker', err);
+    .fail(error => {
+      console.log('Error AJAX', error);
+      // console.log(JSON.parse(localStorage.getItem('lista')));
+      listaDeCompras = JSON.parse(localStorage.getItem('lista'));
+      renderLista();
+    })
+    .always(() => {
+      console.log('Fin de transferencia');
     });
 }
 
-let listaDeCompras = [
-  { nombre: 'Fideos', precio: 1 },
-  { nombre: 'Arroz', precio: 2 },
-  { nombre: 'Harina', precio: 3 },
-  { nombre: 'Aceite', precio: 4 },
-];
-
 function renderLista() {
-  let productos = '';
-
-  listaDeCompras.forEach((producto, index) => {
-    productos += `<li class="mdl-list__item">
-                    <span class="mdl-list__item-primary-content">
-                      ${producto.nombre}
-                    </span>
-    
-                    <div class="mdl-textfield mdl-js-textfield">
-                      <input class="mdl-textfield__input" value="${
-                        producto.precio
-                      }" 
-                        onchange="cambiarPrecio(this,${index})" type="text" id="sample${index +
-      1}">
-                      <label class="mdl-textfield__label" for="sample${index +
-                        1}"></label>
-                    </div>
-    
-                    <button onclick="borrar(${index})" class="boton-borrar mdl-button mdl-js-button mdl-button--raised        mdl-js-ripple-effect">
-                      Borrar
-                    </button>
-                  </li>`;
-  });
-
-  document.getElementById('lista').innerHTML = productos;
+  var source = $('#listaTemplate').html();
+  var template = Handlebars.compile(source);
+  var data = {
+    listaDeCompras,
+  };
+  $('#lista').html(template(data));
 }
 
 function borrar(index) {
@@ -57,27 +93,84 @@ function cambiarPrecio(e, index) {
   let precioNuevo = Number(e.value);
   console.log(e.value);
   listaDeCompras[index] = precioNuevo;
-  console.log(listaDeCompras);
+  // console.log(listaDeCompras);
 }
 
-document.getElementById('boton-borrar').addEventListener('click', () => {
-  console.log('Click al botón borrar');
+function configListeners() {
+  $('#boton-borrar').click(() => {
+    console.log('Click al botón borrar');
 
-  listaDeCompras = [];
+    listaDeCompras = [];
 
-  renderLista();
-});
+    renderLista();
+  });
 
-document.getElementById('boton-agregar').addEventListener('click', () => {
-  console.log('Click al botón agregar');
+  // document.getElementById('boton-agregar').addEventListener('click', () => {
+  $('#boton-agregar').click(() => {
+    console.log('Click al botón agregar');
 
-  let producto = document.getElementById('producto').value;
-  if (producto) {
-    listaDeCompras.push({ nombre: producto, precio: 0 });
-    document.getElementById('producto').value = '';
-  }
+    // let producto = document.getElementById('producto').value;
+    let producto = $('#producto').val();
+    if (producto) {
+      listaDeCompras.push({ producto, precio: 0 });
+      // document.getElementById('producto').value = '';
+      $('#producto').value = '';
+    }
 
-  renderLista();
-});
+    renderLista();
+  });
+}
 
-renderLista();
+// localStorage.setItem('lista', 'lista de compras 01');
+// document.querySelector('.prueba-jquery').innerHTML = '<i>Texto nuevo</i>';
+// document.querySelector('.prueba-jquery').style.color = 'red';
+// $('.prueba-jquery').html('<i>Texto nuevo</i>');
+// $('.prueba-jquery').css('color', 'red');
+
+setServiceWorker();
+getDatosLista();
+configListeners();
+// renderLista();
+
+// ----------------
+// Ejemplo promises
+// ----------------
+// let promesa1 = new Promise((resolve, reject) => {
+//   setTimeout(resolve, 1000, 'One');
+// });
+// let promesa2 = new Promise((resolve, reject) => {
+//   setTimeout(resolve, 2000, 'Two');
+// });
+// let promesa3 = new Promise((resolve, reject) => {
+//   setTimeout(resolve, 3000, 'Three');
+// });
+// let promesa4 = new Promise((resolve, reject) => {
+//   setTimeout(resolve, 4000, 'Four');
+// });
+// let promesa5 = new Promise((resolve, reject) => {
+//   reject('RECHAZADO!');
+// });
+
+// console.log('Promise start');
+
+// Promise.all([promesa1, promesa2, promesa3, promesa4, promesa5])
+//   .then(values => {
+//     console.log(values);
+//   })
+//   .catch(error => console.log(error));
+
+// promesa1.then(valor => {
+//   console.log(valor);
+// });
+
+// promesa2.then(valor => {
+//   console.log(valor);
+// });
+
+// promesa3.then(valor => {
+//   console.log(valor);
+// });
+
+// promesa4.then(valor => {
+//   console.log(valor);
+// });
--- sw.js
+++ sw.js
@@ -1,30 +1,126 @@
-const version = '1.0.0';
-const cacheName = `miappuser-${version}`;
+// Eventos Service Worker
+// 'install', 'activate', 'push', 'fetch', 'sync', 'message'
 
-self.addEventListener('install', e => {
-  console.log('sw install');
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache
-        .addAll(['/', 'index.html', 'js/main.js'])
-        .then(() => self.skipWaiting());
-    }),
-  );
+const CACHE_STATIC_NAME = 'static-v1';
+const CACHE_DYNAMIC_NAME = 'dynamic-v1';
+const CACHE_IMMUTABLE_NAME = 'immutable-v1';
+
+self.addEventListener('install', event => {
+  console.log('SW install');
+
+  const cacheStatic = caches.open(CACHE_STATIC_NAME).then(cache => {
+    // App shell - lo necesario para que la app funcione offline
+    return cache.addAll([
+      '/',
+      '/index.html',
+      '/css/style.css',
+      '/js/main.js',
+      '/images/supermercado.jpg',
+      'https://fonts.googleapis.com/icon?family=Material+Icons',
+    ]);
+  });
+
+  const cacheImmutable = caches.open(CACHE_IMMUTABLE_NAME).then(cache => {
+    // App shell - recursos versionados que con cambian
+    return cache.addAll([
+      'https://code.jquery.com/jquery-3.4.1.min.js',
+      '/js/handlebars-v4.3.1.js',
+      'https://code.getmdl.io/1.3.0/material.indigo-pink.min.css',
+      'https://code.getmdl.io/1.3.0/material.min.js',
+    ]);
+  });
+
+  event.waitUntil(Promise.all([cacheStatic, cacheImmutable]));
 });
 
 self.addEventListener('activate', event => {
-  console.log('sw activate');
-  event.waitUntil(self.clients.claim());
+  console.log('SW activate');
+
+  let cacheWhiteList = [
+    CACHE_STATIC_NAME,
+    CACHE_DYNAMIC_NAME,
+    CACHE_IMMUTABLE_NAME,
+  ];
+
+  caches.keys().then(cacheNames => {
+    return Promise.all(
+      cacheNames.map(cacheName => {
+        if (cacheWhiteList.indexOf(cacheName) === -1) {
+          return caches.delete(cacheName);
+        }
+      }),
+    );
+  });
 });
 
 self.addEventListener('fetch', event => {
-  console.log('sw fetch');
-  event.respondWith(
-    caches
-      .open(cacheName)
-      .then(cache => cache.match(event.request, { ignoreSearch: true }))
-      .then(response => {
-        return response || fetch(event.request);
-      }),
-  );
+  // console.log(event.request.url);
+
+  const respuesta = caches.match(event.request).then(res => {
+    if (res) {
+      console.log(event.request.url, 'Encontrado!');
+      return res;
+    }
+    console.log(event.request.url, 'NO encontrado!');
+
+    return fetch(event.request).then(newRes => {
+      caches.open(CACHE_DYNAMIC_NAME).then(cache => {
+        cache.put(event.request, newRes);
+      });
+      return newRes.clone();
+    });
+  });
+
+  event.respondWith(respuesta);
+
+  // let requerimiento = event.request;
+  // event.respondWith(fetch(requerimiento));
+
+  // if (event.request.url.includes('supermercado.jpg')) {
+  //   // console.log(event.request);
+  //   let fotoReq = event.respondWith(fetch('images/supermercadoT.jpg'));
+  //   event.respondWith(fotoReq);
+  // } else if (event.request.url.includes('style.css')) {
+  //   let respuesta = new Response(
+  //     `main {
+  //       padding: 10px;
+  //     }
+
+  //     .mdl-button {
+  //       margin-left: 30px;
+  //     }
+
+  //     .boton-borrar {
+  //       font-size: 0.7em;
+  //       padding: 0;
+  //       background-color: red;
+  //       color: white;
+  //     }
+
+  //     .boton-borrar:hover {
+  //       background-color: white;
+  //       color: red;
+  //     }
+
+  //     .mdl-textfield__input,
+  //     .mdl-textfield__label {
+  //       margin-left: 30px;
+  //       margin-right: 30px;
+  //       width: 80%;
+  //     }
+
+  //     img {
+  //       border-radius: 50%;
+  //     }`,
+  //     {
+  //       headers: {
+  //         'content-type': 'text/css',
+  //       },
+  //     },
+  //   );
+
+  //   event.respondWith(respuesta);
+  // } else {
+  //   event.respondWith(fetch(event.request));
+  // }
 });
